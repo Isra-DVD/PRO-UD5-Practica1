ac6f96a4432b6883ed75fdc70f41361f14eda5ec
Isra-DVD <usr3849@salesianos-lacuesta.net>
2024-04-20 14:22:25 +0100

    feat(App): Modificada clase App para implementar el menu en la clase ejecutable

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import javax.swing.JOptionPane;
+import net.salesianos.Gestor;
+import net.salesianos.Menu;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
+        boolean seguirPreguntando = true;
+
+        while (seguirPreguntando) {
+
+            String opcionUsuario = JOptionPane.showInputDialog(Menu.mostrar());
+
+            switch (opcionUsuario) {
+                case "1":
+                    Gestor.agregarRestaurante();
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                case "Q":
+                    JOptionPane.showMessageDialog(null, "Adios\n");
+                    seguirPreguntando = false;
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Valor invalido, vuelva a intentarlo\n");
+                    break;
+            }
+        }
     }
 }
