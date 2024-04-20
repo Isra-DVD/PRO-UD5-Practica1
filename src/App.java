726df4e32dbde4059143bcb6948fcea57430a4da
Isra-DVD <usr3849@salesianos-lacuesta.net>
2024-04-20 18:19:07 +0100

    feature(App): Implementado en la clase app las funcionalidades mostrar restaurante u verificar no nullo

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 import net.salesianos.Gestor;
 import net.salesianos.Menu;
+import net.salesianos.Validador;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,32 +11,36 @@ public class App {
         while (seguirPreguntando) {
 
             String opcionUsuario = JOptionPane.showInputDialog(Menu.mostrar());
+            if (Validador.noNulo(opcionUsuario)) {
 
-            switch (opcionUsuario) {
-                case "1":
-                    Gestor.agregarRestaurante();
-                    break;
+                switch (opcionUsuario) {
+                    case "1":
+                        Gestor.agregarRestaurante();
+                        break;
 
-                case "2":
+                    case "2":
 
-                    break;
+                        break;
 
-                case "3":
+                    case "3":
+                        Gestor.mostrarRestaurante();
+                        break;
 
-                    break;
+                    case "4":
 
-                case "4":
+                        break;
 
-                    break;
+                    case "Q":
+                        JOptionPane.showMessageDialog(null, "Adios\n");
+                        seguirPreguntando = false;
+                        break;
 
-                case "Q":
-                    JOptionPane.showMessageDialog(null, "Adios\n");
-                    seguirPreguntando = false;
-                    break;
-
-                default:
-                    JOptionPane.showMessageDialog(null, "Valor invalido, vuelva a intentarlo\n");
-                    break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Valor invalido, vuelva a intentarlo\n");
+                        break;
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Valor invalido, vuelva a intentarlo\n");
             }
         }
     }
